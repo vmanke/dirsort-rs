7369286ff2555bbe7ad6b1bd0351f23aa3de8ff6
vmanke <77922288+vmanke@users.noreply.github.com>
2023-09-08 12:37:11 +0200

    reformatted

--- src/main.rs
+++ src/main.rs
@@ -35,10 +35,15 @@ async fn find_largest_dirs(root_dir: PathBuf, tx: mpsc::Sender<(PathBuf, u64)>) 
     for entry in entries {
         let tx = tx.clone();
         let dir = entry.path().to_path_buf();
+
         let task = tokio::spawn(async move {
             let size = get_size(&dir).unwrap(); // Calculate directory size using fs_extra
-            tx.send((dir, size)).await.expect("Send error");
+            
+            tx.send((dir, size))
+                .await
+                .expect("Send error");
         });
+
         tasks.push(task);
     }
 
@@ -57,11 +62,8 @@ async fn main() {
     }
 
     let root_dir = PathBuf::from(&args[1]);
-
     let (tx, mut rx) = mpsc::channel::<(PathBuf, u64)>(32);
-
     let largest_dirs = tokio::spawn(find_largest_dirs(root_dir.clone(), tx));
-
     let mut results: Vec<(PathBuf, u64)> = Vec::new();
 
     while let Some(result) = rx.recv().await {
@@ -69,11 +71,11 @@ async fn main() {
     }
 
     largest_dirs.await.expect("Task error");
-
     results.sort_by(|a, b| b.1.cmp(&a.1));
 
     for (i, (dir, size)) in results.iter().take(10).enumerate() {
         let formatted_size = format_size(*size);
+
         println!(
             "{:<2}: {:<40} - {}",
             i + 1,
